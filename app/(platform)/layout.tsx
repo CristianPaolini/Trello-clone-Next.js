5cc13ec49899e2f87283f9470d2b458f8fd3eb44
Cristian Paolini <cristianpaolini3@gmail.com>
2024-01-14 19:45:06 -0300

    feat: Introduce BoardList, Info, and FormPopover components
    This commit adds three new components:
    - BoardList: Displays user boards with a "Create new board" button.
    - Info: Shows organization information.
    - FormPopover: A popover component with a form to create a new board.

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,10 +1,12 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const PlatFormLayout = ({
     children
 }: { children: React.ReactNode; }) => {
     return (
         <ClerkProvider>
+            <Toaster />
             {children}
         </ClerkProvider>
     );
